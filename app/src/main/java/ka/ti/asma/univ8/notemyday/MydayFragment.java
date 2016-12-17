2e49084637596c7e8ba841b2b05aa511f1a30389
Asma C K <kouaouci.asma@gmail.com>
2016-12-17 14:27:07 +0100

    [ProjetAndoird] Ajout d’une liste view dans le Fragment MydayFragment

--- app/src/main/java/ka/ti/asma/univ8/notemyday/MydayFragment.java
+++ app/src/main/java/ka/ti/asma/univ8/notemyday/MydayFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 /**
@@ -23,7 +25,21 @@ public class MydayFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_myday, container, false);
+        View view = inflater.inflate(R.layout.fragment_myday, container, false);
+
+        String[] elements = {"Santé","Culture","sport"}; // initializer une liste statique
+
+        ListView listView = (ListView) view.findViewById(R.id.listView_fragment_myday); // récupérer la listView du layout
+
+        // créer l'adapteur pour la listeView
+        ArrayAdapter<String> listViewAdapter =  new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                elements
+        );
+        listView.setAdapter(listViewAdapter); //setter l'adapteur a la listeView
+
+        return view;
     }
 
 }
