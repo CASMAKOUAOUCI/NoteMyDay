f501e423e763c1c894a3f46642ad2c72c980a688
Asma C K <kouaouci.asma@gmail.com>
2016-12-17 13:43:35 +0100

    [ProjetAndoird] Creation des fragments  (Mydayfragment,historiquefragment,grapheFragment)

--- app/src/main/java/ka/ti/asma/univ8/notemyday/MainActivity.java
+++ app/src/main/java/ka/ti/asma/univ8/notemyday/MainActivity.java
@@ -3,6 +3,7 @@ package ka.ti.asma.univ8.notemyday;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -79,19 +80,14 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        FragmentManager fragmentManager = getSupportFragmentManager();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_fragment_myday) {
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new MydayFragment()).commit();
+        } else if (id == R.id.nav_fragment_days_history) {
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new DaysHistory()).commit();
+        } else if (id == R.id.nav_fragment_graphe) {
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new GrapheFragment()).commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
